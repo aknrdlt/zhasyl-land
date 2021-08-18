addb4f9155eef626654e8f46132d193495f32a2c
Aknurdaulet <aknrdlt@gmail.com>
2021-08-18 10:02:16 +0600

    fdsf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -143,68 +143,51 @@
 <section class="steps">
     <div class="container">
         <div class="steps__inner">
-            <div class="suptitle">{{$desc_applications[0]['word_' . app() -> getLocale()]}} </div>
-            <div class="title">{{$desc_applications[1]['word_' . app() -> getLocale()]}}</div>
+            <div class="suptitle">{{$desc_job_order[0]['word_' . app() -> getLocale()]}} </div>
+            <div class="title">{{$desc_job_order[1]['word_' . app() -> getLocale()]}}</div>
             <div class="steps__items">
                 <div class="steps__item steps__item-arrow">
                     <div class="steps__item-icon">
                         <img src="img/steps1.svg" alt="" />
                         <div class="steps__item-count">1</div>
                     </div>
-                    {{$job_order[0]['word_' . app() -> getLocale()]}}                </div>
+                    {{$dec_job_order[0]['word_' . app() -> getLocale()]}}
+                </div>
                 <div class="steps__item steps__item-arrow">
                     <div class="steps__item-icon">
                         <img src="img/steps2.svg" alt="" />
                         <div class="steps__item-count">1</div>
                     </div>
-                    {{$job_order[1]['word_' . app() -> getLocale()]}}                </div>
+                    {{$dec_job_order[1]['word_' . app() -> getLocale()]}}
+                </div>
                 <div class="steps__item">
                     <div class="steps__item-icon">
                         <img src="img/steps3.svg" alt="" />
                         <div class="steps__item-count">1</div>
                     </div>
-                    {{$job_order[2]['word_' . app() -> getLocale()]}}                </div><div class="steps__item steps__item-arrow">
-                    <div class="steps__item-icon">
-                        <img src="img/steps2.svg" alt="" />
-                        <div class="steps__item-count">1</div>
-                    </div>
-                    {{$job_order[1]['word_' . app() -> getLocale()]}}                </div>
-                <div class="steps__item">
-                    <div class="steps__item-icon">
-                        <img src="img/steps3.svg" alt="" />
-                        <div class="steps__item-count">1</div>
-                    </div>
-                    {{$job_order[2]['word_' . app() -> getLocale()]}}                </div>
+                    {{$dec_job_order[2]['word_' . app() -> getLocale()]}}
+                </div>
                 <div class="steps__item steps__item-arrow">
                     <div class="steps__item-icon">
                         <img src="img/steps4.svg" alt="" />
                         <div class="steps__item-count">1</div>
                     </div>
-                    {{$job_order[3]['word_' . app() -> getLocale()]}}                </div>
+                    {{$dec_job_order[3]['word_' . app() -> getLocale()]}}
+                </div>
                 <div class="steps__item steps__item-arrow">
                     <div class="steps__item-icon">
                         <img src="img/steps5.svg" alt="" />
                         <div class="steps__item-count">1</div>
                     </div>
-                    {{$job_order[4]['word_' . app() -> getLocale()]}}                </div>
-                <div class="steps__item steps__item-arrow">
-                    <div class="steps__item-icon">
-                        <img src="img/steps4.svg" alt="" />
-                        <div class="steps__item-count">1</div>
-                    </div>
-                    {{$job_order[3]['word_' . app() -> getLocale()]}}                </div>
-                <div class="steps__item steps__item-arrow">
-                    <div class="steps__item-icon">
-                        <img src="img/steps5.svg" alt="" />
-                        <div class="steps__item-count">1</div>
-                    </div>
-                    {{$job_order[4]['word_' . app() -> getLocale()]}}                </div>
+                    {{$dec_job_order[4]['word_' . app() -> getLocale()]}}
+                </div>
                 <div class="steps__item">
                     <div class="steps__item-icon">
                         <img src="img/steps6.svg" alt="" />
                         <div class="steps__item-count">1</div>
                     </div>
-                    {{$job_order[5]['word_' . app() -> getLocale()]}}                </div>
+                    {{$dec_job_order[5]['word_' . app() -> getLocale()]}}
+                </div>
             </div>
         </div>
     </div>
