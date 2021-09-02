e9d2795f0b572a65a6361058fd64168029ea7d1e
Aknurdaulet Sarkytbayev <63612206+aknrdlt@users.noreply.github.com>
2021-09-02 13:05:13 +0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
     </audio>
 <script>
 const body = document.querySelector('body');
-body.addEventListener('mouseenter',function(){
+body.addEventListener('mousemove',function(){
 document.querySelector('#audioPlay').play();
     document.querySelector('#audioPlay').muted = false;
 });
