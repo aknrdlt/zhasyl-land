03e4ff4df83787553c0c4168002038cb8714b626
Aknurdaulet Sarkytbayev <63612206+aknrdlt@users.noreply.github.com>
2021-09-02 12:32:23 +0500

    fsdf

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -44,7 +44,7 @@ class MainController extends Controller
         
         
         $music = Music::find(2);
-        $link_music = substr(music->music_name, 19, 40);
+        $link_music = substr($music->music_name, 19, 40);
         
         $images_instruments = ImagesInstruments::all();
         $images_letter = ImagesLetter::all();
