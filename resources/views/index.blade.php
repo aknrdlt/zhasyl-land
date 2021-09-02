bdb1d28bf7d22a53b281554beeb8b5cb57e286d2
Aknurdaulet Sarkytbayev <63612206+aknrdlt@users.noreply.github.com>
2021-09-02 13:03:43 +0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@ const body = document.querySelector('body');
 body.addEventListener('click',function(){
     document.querySelector('#audioPlay').play();
 });
-body.dispatchEvent(new Event('click'));
+body.click();
 </script>
 <header class="header">
     <div class="container">
